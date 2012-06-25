b80002998571622bfc5f6efef0ade08c4f9fae91
Florian Bersier <info@xpressyoo.com>
2012-06-25 14:31:24 +0100

    deleted:    chrome/jquery.min.js 	modified:   chrome/manifest.json 	modified:   chrome/redirect.js

--- chrome/redirect.js
+++ chrome/redirect.js
@@ -1,24 +1,3 @@
-chrome.extension.sendRequest({setIconPath: "icon.png"}, function(){});
-
-$(function() {
-    replaceOpenGraphTags();
-
-    $(document).bind("DOMSubtreeModified",replaceOpenGraphTags);
-});
-
-function replaceOpenGraphTags() {
-    $('a[href*="connect/uiserver.php"]').each(function(index, element) {
-        var redirectURI = getURLParameterByName($(element).attr('href'),"redirect_uri");
-        
-        if(redirectURI) {
-            $(element).attr("href",redirectURI);
-            $(element).removeAttr("rel");
-            $(element).attr("target","_blank");        
-        }
-    });
-}
-
-
 function getURLParameterByName(url, name) {
   name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
   var regexS = "[\\?&]"+name+"=([^&#]*)";
@@ -28,3 +7,35 @@ function getURLParameterByName(url, name) {
   if(results == null) return null;
   else return decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+
+function replaceOpenGraphTags() {
+	var links, numLinks, i, redirectURI;
+
+	links = document.querySelectorAll('a[href*="connect/uiserver.php"]');
+	numLinks = links.length;
+
+	for (i = 0; i < numLinks; i++) {
+		redirectURI = getURLParameterByName( links[i].getAttribute('href'), "redirect_uri");
+		if (redirectURI) {
+            links[i].setAttribute('href', redirectURI);
+            links[i].removeAttribute('rel');
+            links[i].setAttribute('target', '_blank');
+        }
+	}
+
+}
+
+
+chrome.extension.sendRequest({setIconPath: "icon.png"}, function () {});
+
+
+
+document.addEventListener('DOMContentLoaded', function (evt) {
+
+	replaceOpenGraphTags();
+	document.addEventListener('DOMSubtreeModified', replaceOpenGraphTags, false);
+
+
+
+}, false);
